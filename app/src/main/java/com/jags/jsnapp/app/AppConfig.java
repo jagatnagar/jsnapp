3e97d2742ad9c41f7abbde3576bd3ed42a59df66
jagatnagar <jagat.nagar@gmail.com>
2016-02-11 04:03:28 +0530

    android app

---
+++ app/src/main/java/com/jags/jsnapp/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.jags.jsnapp.app;
+
+/**
+ * Created by JSN on 2/6/2016.
+ */
+public class AppConfig {
+        // Server user login url
+        public static String URL_LOGIN = "http://10.0.3.2:81/jsnapi/v1/login";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://10.0.3.2:81/jsnapi/v1/register";
+}
---
+++ app/src/main/java/com/jags/jsnapp/helper/Register.java
@@ -0,0 +1,8 @@
+package com.jags.jsnapp.helper;
+
+/**
+ * Created by JSN on 2/11/2016.
+ */
+public class Register {
+
+}
